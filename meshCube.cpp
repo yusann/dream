8903f8987ddfea2db204842b3822e7172e75b983
ka.yuusan <hrskys253@gmail.com>
2017-11-15 19:57:51 +0900

    Cubeの最適化

--- meshCube.cpp
+++ meshCube.cpp
@@ -41,7 +41,7 @@ void CMeshCube::Init(void)
 
 	// プライベート変数の初期化
 	m_pIdxBuff = NULL;
-	m_PolygonNum = 16;
+	m_PolygonNum = 12;
 	m_VexNum = 8;
 
 	// 頂点設定
@@ -98,11 +98,6 @@ void CMeshCube::Draw()
 //=======================================================================================
 void CMeshCube::MakeVex(void)
 {
-	// 変数宣言
-	int nCntHeight;     // 地面ループ用
-	float fLength = sqrt((m_Scl.x * m_Scl.x) + (m_Scl.z * m_Scl.z)) * 0.5f;
-	float fAngle = atan2(m_Scl.z, m_Scl.x);
-
 	// デバイスの取得
 	LPDIRECT3DDEVICE9 pDevice = NULL;
 	pDevice = CManager::GetRenderer()->GetDevice();
@@ -124,36 +119,13 @@ void CMeshCube::MakeVex(void)
 
 	// 頂点バッファをロックして、仮想アドレスを取得する（0,0を記入すると全部をロック）
 	m_pVtxBuff->Lock(0, 0, (void**)&pVtx, 0);
-
-	for (nCntHeight = 0; nCntHeight < 2; nCntHeight++, pVtx += 4)
+	for (int i = 0; i < 2; i++, pVtx += 4)
 	{
 		// 頂点情報の設定
-		/*
-		pVtx[0].pos = D3DXVECTOR3(cosf(-fAngle + D3DX_PI) * fLength,		// X座標の設定
-			nCntHeight * m_Scl.y- (m_Scl.y*0.5f),						// Y座標の設定
-			sinf(-fAngle + D3DX_PI) * fLength);							// Z座標の設定
-		pVtx[1].pos = D3DXVECTOR3(cosf(fAngle) * fLength,					// X座標の設定
-			nCntHeight * m_Scl.y - (m_Scl.y*0.5f),					// Y座標の設定
-			sinf(fAngle) * fLength);									// Z座標の設定
-		pVtx[2].pos = D3DXVECTOR3(cosf(-fAngle) * fLength,					// X座標の設定
-			nCntHeight * m_Scl.y - (m_Scl.y*0.5f),					// Y座標の設定
-			sinf(-fAngle) * fLength);									// Z座標の設定
-		pVtx[3].pos = D3DXVECTOR3(cosf(fAngle + D3DX_PI) * fLength,			// X座標の設定
-			nCntHeight * m_Scl.y - (m_Scl.y*0.5f),					// Y座標の設定
-			sinf(fAngle + D3DX_PI) * fLength);							// Z座標の設定
-			*/
-		pVtx[0].pos = D3DXVECTOR3(cosf(-fAngle + D3DX_PI) * fLength,		// X座標の設定
-			nCntHeight * m_Scl.y,						// Y座標の設定
-			sinf(-fAngle + D3DX_PI) * fLength);							// Z座標の設定
-		pVtx[1].pos = D3DXVECTOR3(cosf(fAngle) * fLength,					// X座標の設定
-			nCntHeight * m_Scl.y,					// Y座標の設定
-			sinf(fAngle) * fLength);									// Z座標の設定
-		pVtx[2].pos = D3DXVECTOR3(cosf(-fAngle) * fLength,					// X座標の設定
-			nCntHeight * m_Scl.y,					// Y座標の設定
-			sinf(-fAngle) * fLength);									// Z座標の設定
-		pVtx[3].pos = D3DXVECTOR3(cosf(fAngle + D3DX_PI) * fLength,			// X座標の設定
-			nCntHeight * m_Scl.y,					// Y座標の設定
-			sinf(fAngle + D3DX_PI) * fLength);							// Z座標の設定
+		pVtx[0].pos = D3DXVECTOR3(-0.5f, i * m_Scl.y,  0.5);
+		pVtx[1].pos = D3DXVECTOR3( 0.5f, i * m_Scl.y,  0.5);
+		pVtx[2].pos = D3DXVECTOR3( 0.5f, i * m_Scl.y, -0.5);
+		pVtx[3].pos = D3DXVECTOR3(-0.5f, i * m_Scl.y, -0.5);
 
 		pVtx[0].normal = D3DXVECTOR3(0.0f, 0.0f, -1.0f);                          // 法線の設定
 		pVtx[1].normal = D3DXVECTOR3(0.0f, 0.0f, -1.0f);                          // 法線の設定
@@ -165,15 +137,14 @@ void CMeshCube::MakeVex(void)
 		pVtx[2].color = m_Color;                       // カラーの設定
 		pVtx[3].color = m_Color;                       // カラーの設定
 
-		pVtx[0].tex = D3DXVECTOR2(0.25f*0.0f, (1.0f/3)*(2-nCntHeight));            // テクスチャ座標の設定
-		pVtx[1].tex = D3DXVECTOR2(0.25f*1.0f, (1.0f/3)*(2-nCntHeight));            // テクスチャ座標の設定
-		pVtx[2].tex = D3DXVECTOR2(0.25f*2.0f, (1.0f/3)*(2-nCntHeight));            // テクスチャ座標の設定
-		pVtx[3].tex = D3DXVECTOR2(0.25f*3.0f, (1.0f/3)*(2-nCntHeight));            // テクスチャ座標の設定
+		pVtx[0].tex = D3DXVECTOR2(0.25f*0.0f, (1.0f/3)*(2-i));            // テクスチャ座標の設定
+		pVtx[1].tex = D3DXVECTOR2(0.25f*1.0f, (1.0f/3)*(2-i));            // テクスチャ座標の設定
+		pVtx[2].tex = D3DXVECTOR2(0.25f*2.0f, (1.0f/3)*(2-i));            // テクスチャ座標の設定
+		pVtx[3].tex = D3DXVECTOR2(0.25f*3.0f, (1.0f/3)*(2-i));            // テクスチャ座標の設定
 	}
 	// 鍵を開ける
 	m_pVtxBuff->Unlock();
 }
-
 
 //=======================================================================================
 // ポリゴンのバッファ設定
@@ -190,7 +161,7 @@ void CMeshCube::MakeBuff(void)
 
 	// インデックスバッファの作成
 	HRESULT hr;
-	hr = pDevice->CreateIndexBuffer(sizeof(WORD) * 18,        // 頂点数（WORDは16Bid）
+	hr = pDevice->CreateIndexBuffer(sizeof(WORD) * (m_PolygonNum+2),        // 頂点数（WORDは16Bid）
 									D3DUSAGE_WRITEONLY,              // 使用用途フラグ（書き込むのみ）
 									D3DFMT_INDEX16,                  // データのフォーマット（32Bid設定した時16を32に変更）
 									D3DPOOL_MANAGED,                 // メモリの管理方法（お任せ）
@@ -202,44 +173,21 @@ void CMeshCube::MakeBuff(void)
 
 	// ロック
 	m_pIdxBuff->Lock(0, 0, (void**)&pIdx, 0);
-/*
-	pIdx[0] = 4;
-	pIdx[1] = 7;
-	pIdx[2] = 5;
-	pIdx[3] = 6;
-	pIdx[4] = 6;
-	pIdx[5] = 2;
-	pIdx[6] = 5;
-	pIdx[7] = 1;
-	pIdx[8] = 4;
-	pIdx[9] = 0;
-	pIdx[10] = 7;
-	pIdx[11] = 3;
-	pIdx[12] = 6;
-	pIdx[13] = 2;
-	pIdx[14] = 2;
-	pIdx[15] = 3;
-	pIdx[16] = 1;
-	pIdx[17] = 0;
-	*/
+
 	pIdx[0] = 4;
 	pIdx[1] = 5;
 	pIdx[2] = 7;
 	pIdx[3] = 6;
 	pIdx[4] = 2;
-	pIdx[5] = 6;
+	pIdx[5] = 5;
 	pIdx[6] = 1;
-	pIdx[7] = 5;
+	pIdx[7] = 4;
 	pIdx[8] = 0;
-	pIdx[9] = 4;
+	pIdx[9] = 7;
 	pIdx[10] = 3;
-	pIdx[11] = 7;
-	pIdx[12] = 2;
-	pIdx[13] = 6;
-	pIdx[14] = 2;
-	pIdx[15] = 1;
-	pIdx[16] = 3;
-	pIdx[17] = 0;
+	pIdx[11] = 2;
+	pIdx[12] = 0;
+	pIdx[13] = 1;
 
 	// ロック解放
 	m_pIdxBuff->Unlock();
